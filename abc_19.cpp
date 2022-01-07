7a8cb4338b177d043b99d53699e1db19f937453a
Art Molados <art.molados@gmail.com>
2022-01-07 16:58:59 +0300

    Добавьте файлы проекта.

---
+++ abc_19.cpp
@@ -0,0 +1,32 @@
+﻿// abc_19.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+enum class Result
+{
+    SUCCESS,
+    FAIL,
+    ERROR_WRITE,
+    ERROR_READ
+};
+
+enum class Another
+{
+    WORK,
+    SUCCESS,
+    FAIL
+};
+
+Another DoWork()
+{
+    return Another::FAIL;
+}
+
+
+
+int main()
+{
+    DoWork();
+}
+
